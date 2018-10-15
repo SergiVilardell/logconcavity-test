2568c8b2a2da7075276611201a1af246be5007ca
SergiVilardell <sergivilardel13@gmail.com>
2018-10-15 18:43:50 +0200

    h updated

--- kde.R
+++ kde.R
@@ -12,10 +12,12 @@ sample <-  rweibull(10000, 3)
 
 #Give values to estimation function
 x_grid <- seq(min(sample), max(sample), length.out = 512)
-h <- 1.06*sd(sample)*length(sample)^(-1/5)
+
+#Maximal smoothing principle window parameter
+h <- 3*sd(sample)*(0.28/(35*length(sample)))^(1/5)
 dens_sample<- kde_cpp(sample, x_grid, h)
 
 #Plot results
 hist(sample, probability = T, breaks = 200)
 lines(x_grid,dens_sample)
-
+density
