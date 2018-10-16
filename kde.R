9972addd746c30b040746a7ca53ae3be704a3475
SergiVilardell <sergivilardell13@gmail.com>
2018-10-16 17:13:02 +0200

    nyi

--- kde.R
+++ kde.R
@@ -15,9 +15,9 @@ x_grid <- seq(min(sample), max(sample), length.out = 512)
 
 #Maximal smoothing principle window parameter
 h <- 3*sd(sample)*(0.28/(35*length(sample)))^(1/5)
-dens_sample<- kde_cpp(sample, x_grid, h)
+dens_sample<- kde_cpp(sample, x_grid, 0.01)
 
+par(mfrow=c(1,2))
 #Plot results
-hist(sample, probability = T, breaks = 200)
-lines(x_grid,dens_sample)
-density
+hist(sample, probability = T, breaks = 100, main = "KDE with h=0.01")
+lines(x_grid,dens_sample, col = "red")
